aef1b431aeee2cbe2232374fa1d6890b96874a91
sinorang <sinholang@naver.com>
2025-07-04 11:24:46 +0900

    네비게이션 바 게시판 토글 기능 개선
    - LoginInterceptor에 BoardService 의존성 추가
    - 모든 페이지에서 게시판 목록을 allBoards 모델 속성으로 전달
    - base.html에서 게시판 토글에 실제 게시판 목록 동적 표시
    - 인증된 사용자와 익명 사용자 모두 적용
    - 게시판 목록과 개별 게시판으로 바로 이동 가능한 UX 개선

--- src/main/java/com/elice/boardproject/security/LoginInterceptor.java
+++ src/main/java/com/elice/boardproject/security/LoginInterceptor.java
@@ -1,18 +1,24 @@
 package com.elice.boardproject.security;
 
 import com.elice.boardproject.acc.entity.User;
+import com.elice.boardproject.board.entity.Board;
+import com.elice.boardproject.board.service.BoardService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
+    
+    @Autowired
+    private BoardService boardService;
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
@@ -22,6 +28,10 @@ public class LoginInterceptor implements HandlerInterceptor {
                 modelAndView.addObject("loginId", loginUser.getId());
                 modelAndView.addObject("loginNickname", loginUser.getNickname());
             }
+            
+            // 모든 페이지에서 게시판 목록을 모델에 추가
+            List<Board> allBoards = boardService.getAllBoards();
+            modelAndView.addObject("allBoards", allBoards);
         }
     }
 } 
